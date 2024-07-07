b2eaf22d6ee721314a79121c0815a1106abfe045
PaulShish <23102628@usc.edu.ph>
2024-07-07 23:23:04 +0800

    Add files via upload

---
+++ IM2-main/increment/account.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style_acc.css" />
+    <title>Increment Technologies</title>
+  </head>
+  <body>
+    <div id="left" class="splitleft">
+      <div id="increment">
+        <h1 class="incre">incre</h1>
+        <h1 class="ment">ment</h1>
+      </div>
+      <div id="bank">
+        <p>XXX Bank | XX000X000</p>
+        <p>₱ 20,000.00</p>
+        <p>Current Balance</p>
+      </div>
+
+      <div id="up">
+      <div id="attributes">
+        <div class="container">
+          <div class="attributes">
+            <img src="img/dashboard.png" alt="dashboard" />
+          </div>
+          <div class="text">
+            <p>Dashboard</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="container">
+          <div class="attributes">
+          <img src="img/insights.png" alt="insights" />
+        </div>
+        <div class="text">
+          <p>Insight</p>
+        </div>
+      </div>
+
+      <div class="container">
+        <div class="attributes">
+          <img src="img/transactions.png" alt="transaction" />
+        </div>
+        <div class="text">
+          <p>Transaction</p>
+      </div>
+    </div>
+
+    <div class="container">
+        <div class="attributes">
+          <img src="img/account.png" alt="account" />
+        </div>
+        <div class="text">
+          <p>Account</p>
+      </div>
+      </div>
+
+      <div class="container">
+        <div class="attributes">
+          <img src="img/settings.png" alt="settings" />
+        </div>
+        <div class="text">
+          <p>Settings</p>
+      </div>
+      </div>
+    </div>
+        <hr class="solid">
+
+      
+      <div id="attributes2">
+        <div class="container">
+        <div class="attributes">
+          <img src="img/add user.png" alt="add account" />
+        </div>
+        <div class="text">
+          <p>Add Account</p>
+      </div>
+      </div>
+
+      <div class="container">
+        <div class="attributes">
+          <img src="img/switch.png" alt="switch account" />
+        </div>
+        <div class="text">
+          <p>Switch Account</p>
+      </div>
+      </div>
+
+      <div class="container">
+        <div class="attributes">
+          <img src="img/logout.png" alt="logout" />
+        </div>
+        <div class="text">
+          <p>Logout</p>
+          
+      </div>
+      </div>
+    </div>
+    </div>
+
+
+    <div id="right" class="splitright">
+      <div class="c2"></div>
+      <div class="border"><div class="line"></div></div>
+    </div>
+
+    
+    
+
+    
+  </body>
+</html>
---
+++ IM2-main/increment/connect.php
@@ -0,0 +1,12 @@
+<?php
+$server = "localhost";
+$user = "root";
+$password = "";
+$dbname = "increment_db";
+
+$conn = new mysqli($server, $user, $password, $dbname);
+if($conn->connect_error){
+    echo "$conn->connect_error";
+    die('Connection Failed :' . $conn->connect_error);
+}
+?>
